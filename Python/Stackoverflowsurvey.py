ca448e7620363aadfa8beff71ea7ee3903d7c548
chandramohank <chandramohan.kontu@gmail.com>
2018-01-04 20:10:36 +0530

    added survey

---
+++ Python/Stackoverflowsurvey.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+surveyData = pd.read_csv("C:/Users/chandramohan/Downloads/survey_results_public.csv")
+surveyData.head()
+surveyData.shape
+surveyData.columns
+
+width = 0.35
+fig, ax = plt.subplots()
+ 
+surveyData['Country'].value_counts()[:10].plot.bar(width=0.8,color='#f45c42')
+ax.patches
--- Python/linear ression using state models.py
+++ Python/linear ression using state models.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import statsmodels.stats as stats
 import seaborn as sns
 import statsmodels.formula.api as smf
+import numpy as np
+
 
 data = pd.read_csv("https://onlinecourses.science.psu.edu/stat501/sites/onlinecourses.science.psu.edu.stat501/files/data/wordrecall.txt",sep='\t',  lineterminator='\n')
 data.head()
@@ -13,19 +15,24 @@ data_y=pd.DataFrame(data.prop)
 plt.plot(data.time,data.prop)
 regr=linear_model.LinearRegression()
 fitres=regr.fit(data_x,data_y)
-visualizaer=ResidualsPlot(regr)
-visualizaer.fit(data_x,data_y)
 
-plt.plot(data_y,fitres._residues)
-fitres.fit
-
+#plot to see the linearity
+sns.residplot(x="time",y="prop",data=data, lowess=True, color="g")
+sns.lmplot(x="time",y="prop",data=data)
 sns.pairplot(data)
+
 lm = smf.ols(formula="time~prop",data=data).fit()
 print(lm.summary())
 
+#residual Plot
+plt.figure()
+lm.resid.plot.density()
+plt.show()
+print('Residual mean:', np.mean(lm.resid))
 influence=lm.get_influence()   
 resid_student=influence.resid_studentized_external
 (cooks, p) = influence.cooks_distance
 (dffits, p) = influence.dffits
 leverage = influence.hat_matrix_diag 
 sns.regplot(leverage, lm.resid_pearson,  fit_reg=False)
+

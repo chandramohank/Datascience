c77c287984cb9123de52ee2b81bb3af15dc0015c
chandramohank <chandramohan.kontu@gmail.com>
2018-01-07 12:53:55 +0530

    07/01/18

--- Python/Stackoverflowsurvey.py
+++ Python/Stackoverflowsurvey.py
@@ -4,7 +4,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-surveyData = pd.read_csv("C:/Users/chandramohan/Downloads/survey_results_public.csv")
+
+def index_exists(ls, i):
+    return (0 <= i < len(ls)) or (-len(ls) <= i < 0)
+
+#surveyData = pd.read_csv("C:/Users/chandramohan/Downloads/survey_results_public.csv")
+surveyData = pd.read_csv("E:/DataScience/Practice/Datascience/Python/stack-overflow-developer-survey-2017/survey_results_public.csv")
 surveyData.head()
 surveyData.shape
 surveyData.columns
@@ -28,3 +33,27 @@ for p in ax.patches:
     ax.annotate(format(p.get_height()),(p.get_x()+0.10,p.get_height()+0.25))
     
 #Most famous languages among students
+    d=[]
+    lst=[]
+    students['HaveWorkedLanguage'].apply(lambda x:d.extend(str(x).split('; ')))
+    lst=list(set(d))
+    
+    dict={}
+    for i in lst:
+        counts=students['HaveWorkedLanguage'].apply(lambda x: 'java' in str(x).split(';')).value_counts()
+        if index_exists(counts,1):
+            dict[i]=counts[1]
+            
+            
+    del dict['nan']
+    lang=pd.DataFrame(list(dict.items()))
+    
+    lang.columns=[['Language','Count']]
+    lang= lang.sort_values(by=['Language'],ascending=[False])  
+    plt.subplots(figsize=(12,6))
+    #lang.set_index('Language',inplace=True)   
+ax=    lang.sort_values(by=['Count'],ascending=[False]).head(10).plot.bar(width=0.8,color='#005544')
+for p in ax.patches:
+    ax.annotate(format(p.get_height()),(p.get_x()+0.10,p.get_height()+0.25))
+
+    

0d349b24c81dede1e682df390d915fcc3b49affc
chandramohank <chandramohan.kontu@gmail.com>
2018-01-25 20:33:38 +0530

    changes 1/25

--- Python/titanic.py
+++ Python/titanic.py
@@ -51,15 +51,24 @@ grid.map(sns.barplot, 'Sex', 'Fare', palette='deep')
 
 for dataset in combine:
     dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+    
 pd.crosstab(train_df['Title'], train_df['Sex'])
+
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+ 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
 
 title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
 for dataset in combine:
     dataset['Title']=dataset['Title'].map(title_mapping)
     dataset['Title']=dataset.fillna(0)
     
- train_df.head()
- train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
+train_df.head()
+train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
 test_df = test_df.drop(['Name'], axis=1)
 combine = [train_df, test_df]
 train_df.shape, test_df.shape
@@ -67,4 +76,26 @@ train_df.shape, test_df.shape
 for dataset in combine:
     dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
     
+#remove nul from AGE
+grid = sns.FacetGrid(train_df, row='Pclass', col='Sex', size=2.2, aspect=1.6)
+grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+grid.add_legend()
+
+guess_ages = np.zeros((2,3))
+guess_ages
+    
+for dataset in combine:
+    for i in range(0, 2):
+        for j in range(0, 3):
+            guess_df = dataset[(dataset['Sex'] == i) & \
+                                  (dataset['Pclass'] == j+1)]['Age'].dropna()
+
+            # age_mean = guess_df.mean()
+            # age_std = guess_df.std()
+            # age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
+
+            age_guess = guess_df.median()
+
+            # Convert random age float to nearest .5 age
+            guess_ages[i,j] = int( age_guess/0.5 + 0.5 ) * 0.5
     
